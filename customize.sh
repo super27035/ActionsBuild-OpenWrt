62089974329cafb600d96770de82be2717ed0a54
super27035 <superssatellite@gmail.com>
2023-09-05 22:28:59 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.50.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+rm openwrt/package/lean/luci-theme-argon -rf
+
+#4.Set DISTRIB_REVISION
+sed -i "s/OpenWrt /lejitao Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
